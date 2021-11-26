1ef940b8ec3ba5a81039369968b7983af37c1ddf
devLucasRoberto <chromegold70@gmail.com>
2021-11-26 17:45:48 -0300

    Add next-react-svg

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: true
 }
+
+const withReactSvg = require('next-react-svg')
+const path = require('path')
+
+module.exports = withReactSvg({
+  include: path.resolve(__dirname, 'src/assets/svg'),
+  webpack(config, options) {
+    return config
+  }
+})
