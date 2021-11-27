88b8683d3dbca3116172617cf17ef77805a83402
devLucasRoberto <chromegold70@gmail.com>
2021-11-27 15:25:02 -0300

    Deleted next-rect-svg

--- next.config.js
+++ next.config.js
@@ -2,13 +2,3 @@
 module.exports = {
   reactStrictMode: true
 }
-
-const withReactSvg = require('next-react-svg')
-const path = require('path')
-
-module.exports = withReactSvg({
-  include: path.resolve(__dirname, 'src/assets/svg'),
-  webpack(config, options) {
-    return config
-  }
-})
