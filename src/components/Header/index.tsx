7841fe64eaa1704138e4f3d65eadfbc2bcc54fa6
devLucasRoberto <chromegold70@gmail.com>
2021-11-27 15:23:18 -0300

    Add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,25 @@
+import { Flex, Box, Image, Link as ChakraLink } from '@chakra-ui/react'
+import Link from 'next/link'
+
+export function Header() {
+  return (
+    <Flex my={27} mx={140}>
+      <Box>
+        <Link href="/" passHref>
+          <ChakraLink>
+            <Image
+              src="/assets/back.svg"
+              alt="back"
+              width="32px"
+              height="32px"
+            />
+          </ChakraLink>
+        </Link>
+      </Box>
+
+      <Box mx="auto">
+        <Image src="/assets/logo.png" alt="worldtrip" />
+      </Box>
+    </Flex>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import { Flex } from '@chakra-ui/react'
+import { Header } from '../components/Header'
+
 export default function Home() {
   return (
-    <>
+    <Flex
+      w="100%"
+      maxWidth={1440}
+      direction="column"
+      mx="auto"
+      minHeight="100hv"
+    >
+      <Header />
       <h1>home</h1>
-    </>
+    </Flex>
   )
 }
