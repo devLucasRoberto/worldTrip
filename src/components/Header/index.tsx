0b5db24a8f5ba9090a4c037b1edef1c31becf264
devLucasRoberto <chromegold70@gmail.com>
2021-11-28 18:05:30 -0300

    Fix header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,36 @@
 import { Flex, Box, Image, Link as ChakraLink } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 export function Header() {
+  const { asPath } = useRouter()
+
   return (
     <Flex my={27} mx={140} alignItems="center">
-      <Box>
-        <Link href="/" passHref>
-          <ChakraLink>
-            <Image
-              src="/assets/back.svg"
-              alt="back"
-              width="32px"
-              height="32px"
-            />
-          </ChakraLink>
-        </Link>
-      </Box>
+      {asPath != '/' && (
+        <Box>
+          <Link href="/" passHref>
+            <ChakraLink
+              _focus={{
+                outline: 'none'
+              }}
+            >
+              <Image
+                src="/assets/back.svg"
+                alt="back"
+                width="32px"
+                height="32px"
+              />
+            </ChakraLink>
+          </Link>
+        </Box>
+      )}
 
-      <Box mx="auto">
-        <Image src="/assets/logo.png" alt="worldtrip" />
-      </Box>
+      <Link href="/" passHref>
+        <Box as="a" mx="auto">
+          <Image src="/assets/logo.png" alt="worldtrip" />
+        </Box>
+      </Link>
     </Flex>
   )
 }
