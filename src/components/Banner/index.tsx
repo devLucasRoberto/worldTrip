5c675f0b04c682dbc8c227db02aa5e39d205c81f
devLucasRoberto <chromegold70@gmail.com>
2021-11-27 21:31:38 -0300

    Fix banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,41 +4,44 @@ export function Banner() {
   return (
     <Box>
       <Box position="relative">
-        <Image src="assets/capa.png" w="100%" />
+        <Image src="assets/capa.png" w="1440px" h="335px" objectFit="cover" />
+
+        <Flex
+          justifyContent="space-between"
+          width="100%"
+          top="0"
+          position="absolute"
+          zIndex="5"
+          maxWidth="1440px"
+          px="140px"
+          height="100%"
+          alignItems="center"
+        >
+          <Box>
+            <Heading
+              color="gray.50"
+              fontSize="36"
+              fontWeight="500"
+              lineHeight="1.5"
+            >
+              5 Continentes, <br /> infinitas possibilidades.
+            </Heading>
+            <Text color="gray.200" fontSize="20px" fontWeight="400" mt="20px">
+              Chegou a hora de tirar do papel a viagem que você <br /> sempre
+              sonhou.
+            </Text>
+          </Box>
+
+          <Box marginTop="120px">
+            <Image
+              src="assets/airplane.svg"
+              maxWidth="417px"
+              maxHeight="270px"
+              width="100%"
+            />
+          </Box>
+        </Flex>
       </Box>
-
-      <Flex
-        justifyContent="space-between"
-        width="100%"
-        mt="-255"
-        position="absolute"
-        maxWidth="1440px"
-        px="140px"
-      >
-        <Box>
-          <Heading
-            color="gray.50"
-            fontSize="36"
-            fontWeight="500"
-            lineHeight="1.5"
-          >
-            5 Continentes, <br /> infinitas possibilidades.
-          </Heading>
-          <Text color="gray.200" fontSize="20px" fontWeight="400" mt="20px">
-            Chegou a hora de tirar do papel a viagem que você <br /> sempre
-            sonhou.
-          </Text>
-        </Box>
-
-        <Box marginTop="10px">
-          <Image
-            src="assets/airplane.svg"
-            maxWidth="417px"
-            maxHeight="270px"
-            width="100%"
-          />
-        </Box>
-      </Flex>
     </Box>
   )
 }
